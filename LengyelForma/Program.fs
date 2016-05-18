7b2e835f0e407e8901c022d3937a187b79ca1014
zeon <zoen@zeon.hu>
2016-05-18 22:47:06 +0200

    Kód tisztázása

--- LengyelForma/Program.fs
+++ LengyelForma/Program.fs
@@ -20,24 +20,19 @@ type Token =
 // Megadja, hogy a kapott token operátor-e
 let isOperator input =
     match input with
-    | Token.Number x -> false
-    | Token.ParenthesisOpen | Token.ParenthesisClose -> false
+    | Number x -> false
+    | ParenthesisOpen | ParenthesisClose -> false
     | _ -> true
 
 // Visszaadja az operátorok precedenciáját az operátor stack kezeléséhez
 let precedence input =
     match input with
-    | Token.Add | Token.Substract -> 1
-    | Token.Multiply | Token.Divide -> 2
+    | Add | Substract -> 1
+    | Multiply | Divide -> 2
     | _ -> 0
 
 // Pretokeneket tokenné alakítja split segítségével
 let tokenizeWithSplit (input:string) =
-    let stretchedString = input.Replace("+", " + ").Replace("-", " - ").Replace(":", " : ").Replace("/", " : ").Replace(",", ".").Replace(")", " ) ").Replace("(", " ( ").Replace("*", " * ")
-
-    let stringTokens = stretchedString.Split(' ')
-
-    printfn "STOKENS: %A" stringTokens
 
     let identifyToken (inputToken:string) =
         match inputToken with
@@ -50,8 +45,8 @@ let tokenizeWithSplit (input:string) =
         | ""  -> Empty
         | _ -> Number (inputToken |> Double.Parse)
 
-    // Minden karaktert preTokenre konvertál
-    stringTokens |> Seq.map identifyToken
+    let stretchedString = input.Replace("+", " + ").Replace("-", " - ").Replace(":", " : ").Replace("/", " : ").Replace(",", ".").Replace(")", " ) ").Replace("(", " ( ").Replace("*", " * ")
+    stretchedString.Split(' ') |> Seq.map identifyToken
 
 // Tokenekkben leírt infix kifejezés postfix kifejezéssé konvertálása
 let convertToPolishNotation inputTokens =
@@ -60,17 +55,17 @@ let convertToPolishNotation inputTokens =
 
     for token in inputTokens do
         match token with
-        | Token.Empty -> ()
-        | Token.ParenthesisOpen -> stack.Push Token.ParenthesisOpen
-        | Token.ParenthesisClose -> 
+        | Empty -> ()
+        | ParenthesisOpen -> stack.Push ParenthesisOpen
+        | ParenthesisClose -> 
             // A nyitó zárójelig mindent felvenni a PF-be a veremből
-            while (stack.Peek() <> Token.ParenthesisOpen ) do
+            while (stack.Peek() <> ParenthesisOpen ) do
                 let popped = stack.Pop()
                 postfix.Add(popped)
             
             stack.Pop() |> ignore
 
-        | Token.Number x ->  postfix.Add (Token.Number x)
+        | Number x ->  postfix.Add (Number x)
         | _ -> 
             // Műveleti jel: ha van nagyobb precedenciájú operátor, akkor kivenni, utána operátor verembe
             // If token is an operator (x) [S3]:
@@ -101,10 +96,10 @@ let convertToPolishNotation inputTokens =
 // Végrehajtja az operatorban megadott műveletet az op1 és op2 operanduson
 let applyOperator (operator, op1, op2) =
     match operator, op1, op2 with
-        | Token.Add, Token.Number a, Token.Number b -> Token.Number (a + b)
-        | Token.Substract, Token.Number a, Token.Number b -> Token.Number (b - a)
-        | Token.Multiply, Token.Number a, Token.Number b -> Token.Number (a * b)
-        | Token.Divide, Token.Number a, Token.Number b -> Token.Number (b / a)
+        | Add, Number a, Number b -> Number (a + b)
+        | Substract, Number a, Number b -> Number (b - a)
+        | Multiply, Number a, Number b -> Number (a * b)
+        | Divide, Number a, Number b -> Number (b / a)
         | _ -> failwith "Nem kezelt operátor vagy hibás stack"
 
 // Postfix-kifejezés megoldása
@@ -113,32 +108,23 @@ let solvePolishNotation inputTokens =
 
     for token in inputTokens do
         match token with
-        | Token.Number x -> stack.Push (Token.Number x)
+        | Number x -> stack.Push (Number x)
         | _ -> stack.Push (applyOperator (token, stack.Pop(), stack.Pop()))
 
     match stack.Count with 
         | 1 -> stack.Pop()
         | _ -> failwith "Hibás megoldás stack"
 
+let getValue (x) =
+    match x with
+    | Number a -> a
+    | _ -> failwith "Nem szám"
+
 // Belépési pont, expression.txt beolvasása és megoldása
 [<EntryPoint>]
 let main argv = 
 
-    let expression = File.ReadAllText("expression.txt");
-    printfn "Beolvasott kifejezés: %s" expression
+    let solution = File.ReadAllText("expression.txt") |> tokenizeWithSplit |> convertToPolishNotation |> solvePolishNotation
+    printfn "A megoldás: %f" (solution |> getValue)
 
-//    let tokens = tokenize (expression.ToCharArray())
-    let tokens = tokenizeWithSplit (expression)
-    printfn "Tokenek: %A" (Seq.toList tokens)
-
-    let polishTokens = tokens |> convertToPolishNotation
-    printfn "Ezt várjuk: 5 12.2 + 4 3 * 3 / - 5 2 + 4 * + "
-    printfn "postfix-tokenek: %A" (Seq.toList polishTokens)
-
-    printfn "Ezt várjuk: 41.2"
-    let solution = polishTokens |> solvePolishNotation
-    printfn "megoldas: %A" solution
-
-    // TODO: A végén ha nem kell debug az egészet összepipe-ozni
-
-    0 // return an integer exit code
+    0 // 0, mert sikeresen lefutottunk
